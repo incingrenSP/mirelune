2639905777d8d4cd7e822eefb614d3853dcec39e
incingrenSP <samirpokharel002@gmail.com>
2026-09-09 16:38:43 +0545

    hitbox implementation; take damage receive damage

--- scenes/entities/hitbox.gd
+++ scenes/entities/hitbox.gd
@@ -13,3 +13,25 @@ func _ready() -> void:
 		if is_instance_valid(parent):
 			stat_component = parent.get_node_or_null("StatComponent") as StatComponent
 			
+	HitboxRegistry.register(self)
+	
+func _exit_tree() -> void:
+	HitboxRegistry.unregister(self)
+	
+func receive_hit(instigator: Node3D, skill: SkillData, target_data: Dictionary = {}) -> float:
+	var instigator_stats := {}
+	var my_stats := {}
+	
+	if is_instance_valid(instigator):
+		var instigator_stat_component := instigator.get_node_or_null("StatComponent") as StatComponent
+		
+		if is_instance_valid(instigator_stat_component):
+			instigator_stats = instigator_stat_component.get_stats_as_formula_dict()
+	
+	if is_instance_valid(stat_component):
+		my_stats = stat_component.get_stats_as_formula_dict()
+	
+	var damage := skill.calculate_damage(instigator_stats, my_stats)
+	hit_received.emit(instigator, skill, damage, target_data)
+	
+	return damage
